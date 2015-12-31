35409ada24129f02d740815373a7c223a11f4864
Аня Серба <annserba94@gmail.com>
2015-12-31 22:31:15 +0300

    Превью

---
+++ Orchard CMS/Themes/TheThemeMachine/Scripts/Site.js
@@ -0,0 +1,4 @@
+$(".tags a").addClass("label label-warning");
+$(".blog li").addClass("box box-widget widget-user-1");
+$(".blog li header").addClass("widget-user-header");
+$(".blog li header h1 a").addClass("widget-user-username");
